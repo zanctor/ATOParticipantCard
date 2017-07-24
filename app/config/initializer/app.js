b963e9711c64a5911cb52da0735210e47ade3b04
Denys Markov <denys.markov@n3wnormal.com>
2017-07-24 13:29:41 +0300

    added CORS middleware

--- app/config/initializer/app.js
+++ app/config/initializer/app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 const _ = require('lodash');
+const cors = require('./cors');
 
 express.response.jsonSuccess = function (data) {
     this.json({data})
@@ -12,6 +13,7 @@ express.response.errorResponse = function (status, e) {
 };
 
 app.use(jsonParser);
+app.use(cors);
 
 require('./routes')(app);
 
