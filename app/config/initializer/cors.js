49e78b65a20a1ff4a3e077b11a3ea2bd16531fdf
Denys Markov <denys.markov@n3wnormal.com>
2017-07-25 11:27:27 +0300

    added x-access-token type support for response

--- app/config/initializer/cors.js
+++ app/config/initializer/cors.js
@@ -1,5 +1,5 @@
 module.exports = function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Headers", "Content-Type, x-access-token");
     next();
 };
