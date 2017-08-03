4f6cdf595c6787b645826f8e32f82c458fe869b5
Denys Markov <denys.markov@n3wnormal.com>
2017-08-03 15:17:21 +0300

    added PATCH request method cors support

--- app/config/initializer/cors.js
+++ app/config/initializer/cors.js
@@ -1,6 +1,6 @@
 module.exports = function cors(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type, x-access-token");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
     next();
 };
